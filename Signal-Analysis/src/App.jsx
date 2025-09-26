a40f5f010e0bcc3b7e0b5a930d4aa6baa85157ff
Anas <anaselsheikh2004@gmail.com>
2025-09-26 20:56:22 +0300

    pages routing added

--- Signal-Analysis/src/App.jsx
+++ Signal-Analysis/src/App.jsx
@@ -1,9 +1,22 @@
 import Home from "./Home";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EEG from "./../Pages/EEG";
+import ECG from "./../Pages/ECG";
+import Sound from "./../Pages/Sound";
+import Doppler from "./../Pages/Doppler";
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/EEG" element={<EEG />} />
+          <Route path="/ECG" element={<ECG />} />
+          <Route path="/Sound" element={<Sound />} />
+          <Route path="/Doppler" element={<Doppler />} />
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
